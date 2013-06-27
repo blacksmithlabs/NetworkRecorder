db8c4306a815bfadb102ee0d6435d1952f3cccf6
Brian Armstrong <brian@hamroute.com>
2013-06-27 00:01:29 -0600

    Fixing Log Settings activity to properly resume with its fragments

--- Network Recorder/src/main/java/com/blacksmithlabs/networkrecorder/LogSettingsActivity.java
+++ Network Recorder/src/main/java/com/blacksmithlabs/networkrecorder/LogSettingsActivity.java
@@ -1,11 +1,10 @@
 package com.blacksmithlabs.networkrecorder;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +20,10 @@ import java.util.ArrayList;
  */
 public class LogSettingsActivity extends FragmentActivity {
 	final public static String EXTRA_APP = "LogSettings.app";
+
+	final protected static String TITLE_FRAGMENT_TAG = "LogSettings.TitleFragment";
+	final protected static String PORT_FRAGMNET_TAG = "LogSettings.PortFragment";
+	final protected static String PORT_FRAGMENT_COUNT = "LoggetSettings.PortCount";
 
 	protected ApplicationHelper.DroidApp app;
 
@@ -41,7 +44,7 @@ public class LogSettingsActivity extends FragmentActivity {
 		if (app == null) {
 			MessageBox.error(this, getString(R.string.error_settings_noapp));
 
-			Intent main = new Intent(this, MainActivity.class);
+			final Intent main = new Intent(this, MainActivity.class);
 			main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 			startActivity(main);
 
@@ -52,14 +55,27 @@ public class LogSettingsActivity extends FragmentActivity {
 			portSelectors = new ArrayList<PortSelectorFragment>();
 		}
 
+		final FragmentManager fragmentManager = getSupportFragmentManager();
+
 		// If we are not resuming, populate the fragments
 		if (savedInstanceState == null) {
 			titleFragment = new AppTitleFragment();
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.settings_app_container, titleFragment)
+			fragmentManager.beginTransaction()
+					.add(R.id.settings_app_container, titleFragment, TITLE_FRAGMENT_TAG)
 					.commit();
 
-			addPortFragment(null);
+			_addPortFragment(null);
+		} else {
+			titleFragment = (AppTitleFragment)fragmentManager.findFragmentByTag(TITLE_FRAGMENT_TAG);
+
+			final int portCount = savedInstanceState.getInt(PORT_FRAGMENT_COUNT, 0);
+			if (portCount <= 0) {
+				_addPortFragment(null);
+			} else {
+				for (int i=0; i<portCount; i++) {
+					portSelectors.add((PortSelectorFragment)fragmentManager.findFragmentByTag(PORT_FRAGMNET_TAG + i));
+				}
+			}
 		}
 	}
 
@@ -67,6 +83,7 @@ public class LogSettingsActivity extends FragmentActivity {
 	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putParcelable(EXTRA_APP, app);
+		outState.putInt(PORT_FRAGMENT_COUNT, portSelectors.size());
 	}
 
 	@Override
@@ -80,17 +97,19 @@ public class LogSettingsActivity extends FragmentActivity {
 		// TODO load the log activity and give it all the settings so it can start the service and such
 	}
 
-	protected void addPortFragment(String port) {
-		PortSelectorFragment fragment = new PortSelectorFragment();
+	public void _addPortFragment(String port) {
+		final PortSelectorFragment fragment = new PortSelectorFragment();
 		portSelectors.add(fragment);
 
+		final String fragmentTag = PORT_FRAGMNET_TAG + portSelectors.size();
+
 		getSupportFragmentManager().beginTransaction()
-				.add(R.id.settings_ports_container, fragment)
+				.add(R.id.settings_ports_container, fragment, fragmentTag)
 				.commit();
 	}
 
-	protected void removePortFragment(Fragment fragment) {
-		int location = portSelectors.indexOf(fragment);
+	public void _removePortFragment(Fragment fragment) {
+		final int location = portSelectors.indexOf(fragment);
 		if (location >= 0) {
 			portSelectors.remove(location);
 
@@ -101,11 +120,11 @@ public class LogSettingsActivity extends FragmentActivity {
 
 		// Don't let it become empty
 		if (portSelectors.isEmpty()) {
-			addPortFragment(null);
+			_addPortFragment(null);
 		}
 	}
 
-	public class AppTitleFragment extends Fragment {
+	public static class AppTitleFragment extends Fragment {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			super.onCreateView(inflater, container, savedInstanceState);
@@ -113,40 +132,42 @@ public class LogSettingsActivity extends FragmentActivity {
 		}
 
 		public void populateData(ApplicationHelper.DroidApp app) {
-			TextView text = (TextView)getView().findViewById(R.id.app_item_text);
-			ImageView icon = (ImageView)getView().findViewById(R.id.app_item_icon);
+			final TextView text = (TextView)getView().findViewById(R.id.app_item_text);
+			final ImageView icon = (ImageView)getView().findViewById(R.id.app_item_icon);
 
 			text.setText(app.toString());
 			app.loadIcon(getActivity(), icon);
 		}
 	}
 
-	public class PortSelectorFragment extends Fragment {
+	public static class PortSelectorFragment extends Fragment {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			super.onCreateView(inflater, container, savedInstanceState);
-			View view = inflater.inflate(R.layout.settings_port_fragment, container, false);
+			final View view = inflater.inflate(R.layout.settings_port_fragment, container, false);
 
-			ImageView addButton = (ImageView)view.findViewById(R.id.add_port);
+			final ImageView addButton = (ImageView)view.findViewById(R.id.add_port);
 			addButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_add));
 			addButton.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View view) {
-					LogSettingsActivity.this.addPortFragment(null);
+					((LogSettingsActivity)getActivity())._addPortFragment(null);
 				}
 			});
 
-			ImageView removeButton = (ImageView)view.findViewById(R.id.remove_port);
+			final ImageView removeButton = (ImageView)view.findViewById(R.id.remove_port);
 			removeButton.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_delete));
 			removeButton.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View view) {
-					LogSettingsActivity.this.removePortFragment(PortSelectorFragment.this);
+					((LogSettingsActivity)getActivity())._removePortFragment(PortSelectorFragment.this);
 				}
 			});
 
-			TextView text = (TextView)view.findViewById(R.id.port);
-			text.requestFocus(TextView.FOCUS_DOWN);
+			if (savedInstanceState == null) {
+				final TextView text = (TextView)view.findViewById(R.id.port);
+				text.requestFocus(TextView.FOCUS_DOWN);
+			}
 
 			return view;
 		}
